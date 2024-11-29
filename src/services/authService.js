2446b66883efd409278cbf1bd1b995e617eaacb3
Camila Alm <camila.salmeidatech@hotmail.com>
2024-11-29 12:27:17 -0500

    create signin form and signin service

--- src/services/authService.js
+++ src/services/authService.js
@@ -20,8 +20,32 @@ try {
 }
 }
 
+const signin = async (user) => {
+    try {
+        const res = await fetch(`${BACKEND_URL}/users/signin`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(user),
+        })
+        const json = await res.json();
+        console.log(json);
+
+        if (json.error) {
+            throw new Error(json.error);
+        }
+        if (json.token) {
+            const user = JSON.parse(atob(json.token.split('.')[1]));
+            console.log(user);
+            return user;
+        }
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+}
 
 
 export {
     signup,
+    signin,
 };
